eb03009235e5129be5cd9dee675f84a975d151f9
jwischer <jakewischer@gmail.com>
2024-01-29 14:05:00 -0600

    video functionality using pygame rather than cv2

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ import subprocess
 import pygame.camera
 import pygame.image
 
-#Photo size in pixels
-PHOTO_WINDOW_SIZE_X = 600
-PHOTO_WINDOW_SIZE_Y = 400
-
 #Start hotkey functions
 def aPress(): #Move mouse
     pyautogui.move(50, 50, 1, pyautogui.easeInQuad)
@@ -21,9 +17,9 @@ def aPress(): #Move mouse
 def rPress(): #Open file
     subprocess.call(["xdg-open",os.path.join(mainFolder,"README.md")]) #open readme
     
-def pPress(): #Take picture
-    global takePic
-    takePic = True
+def vPress(): #Take video
+    global playVideo
+    playVideo = True
 
 def qPress(): #Close picture window
     global exitWindow
@@ -39,41 +35,33 @@ screenWidth, screenHeight = pyautogui.size() #get monitor resolution
 #Hotkey definitions
 keyboard.add_hotkey('a', lambda: aPress())
 keyboard.add_hotkey('r', lambda: rPress())
-keyboard.add_hotkey('p', lambda: pPress())
+keyboard.add_hotkey('v', lambda: vPress())
 keyboard.add_hotkey('q', lambda: qPress())
 keyboard.add_hotkey('esc', lambda: escPress())
 #Init flags
-needInit = True
 takePic = False
 exitWindow = False
+playVideo = False
 
 while True: #Infinite loop to check for flags
-    if(needInit and takePic):
+    if(playVideo):
         #Initialize camera window
         pygame.camera.init()
         cameras = pygame.camera.list_cameras()
         webcam = pygame.camera.Camera(cameras[0])
-        #grab first frame
         webcam.start()
+        #grab initial frame
         img = webcam.get_image()
-        webcam.stop()
         WIDTH = img.get_width()
         HEIGHT = img.get_height()
         screen = pygame.display.set_mode((WIDTH,HEIGHT), pygame.NOFRAME)
-        pygame.display.set_caption("pyGame Camera View")
-        screen.blit(img, (0,0))
-        pygame.display.flip()
-        pygame.image.save(img, "image.png")
-        needInit = False
-        takePic = False
-    elif(takePic):
-        webcam.start()
-        img = webcam.get_image()
-        screen.blit(img, (0,0))
-        pygame.display.flip()
+        while(not exitWindow):
+            img = webcam.get_image()
+            pygame.display.set_caption("pyGame Camera View")
+            screen.blit(img, (0,0))
+            pygame.display.flip()
         webcam.stop()
-        pygame.image.save(img, "image.png")
-        takePic = False
+        playVideo = False
         
     if(exitWindow):
         pygame.quit()
