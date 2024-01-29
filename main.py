ec6d5417bd2a841dd3f0d5432898ace585eb9aff
jwischer <jakewischer@gmail.com>
2024-01-29 04:15:58 -0600

    swiched cv2 for pygame for better rendering

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 from time import sleep #used for delays for mouse inputs
 import pyautogui
 import keyboard
-import cv2
 import subprocess
+import pygame.camera
+import pygame.image
 
 #Photo size in pixels
 PHOTO_WINDOW_SIZE_X = 600
@@ -21,26 +22,12 @@ def rPress(): #Open file
     subprocess.call(["xdg-open",os.path.join(mainFolder,"README.md")]) #open readme
     
 def pPress(): #Take picture
-    cam = cv2.VideoCapture(0) #Grab camera
-    noError,frame = cam.read() #Read from camera
-    if(noError): #If successful
-        #Open window and change settings
-        cv2.namedWindow('Photo',cv2.WINDOW_GUI_NORMAL)
-        x,y,windowWidth,Windowheight = cv2.getWindowImageRect("Photo")
-        cv2.resizeWindow("Photo",PHOTO_WINDOW_SIZE_X,PHOTO_WINDOW_SIZE_Y)
-        cv2.moveWindow("Photo",int((screenWidth-windowWidth)/2),int((screenHeight-Windowheight)/2))
-        cv2.imshow("Photo",frame) #Show image
-        cv2.waitKey(1)
-        cv2.imwrite("picture.png", frame) #Save image
-        cam.release()
-    else:
-        print("Camera Error")
-        
+    global takePic
+    takePic = True
+
 def qPress(): #Close picture window
-    try:
-        cv2.destroyWindow("Photo")
-    except:
-        print("No window to close")
+    global exitWindow
+    exitWindow = True
     
 def escPress(): #Exit script
     os._exit(0)
@@ -55,6 +42,40 @@ keyboard.add_hotkey('r', lambda: rPress())
 keyboard.add_hotkey('p', lambda: pPress())
 keyboard.add_hotkey('q', lambda: qPress())
 keyboard.add_hotkey('esc', lambda: escPress())
+#Init flags
+needInit = True
+takePic = False
+exitWindow = False
 
-while True: #Infinite loop so script wont close, hotkeys handled in thread
-    pass
+while True: #Infinite loop to check for flags
+    if(needInit and takePic):
+        #Initialize camera window
+        pygame.camera.init()
+        cameras = pygame.camera.list_cameras()
+        webcam = pygame.camera.Camera(cameras[0])
+        #grab first frame
+        webcam.start()
+        img = webcam.get_image()
+        webcam.stop()
+        WIDTH = img.get_width()
+        HEIGHT = img.get_height()
+        screen = pygame.display.set_mode((WIDTH,HEIGHT), pygame.NOFRAME)
+        pygame.display.set_caption("pyGame Camera View")
+        screen.blit(img, (0,0))
+        pygame.display.flip()
+        pygame.image.save(img, "image.png")
+        needInit = False
+        takePic = False
+    elif(takePic):
+        webcam.start()
+        img = webcam.get_image()
+        screen.blit(img, (0,0))
+        pygame.display.flip()
+        webcam.stop()
+        pygame.image.save(img, "image.png")
+        takePic = False
+        
+    if(exitWindow):
+        pygame.quit()
+        exitWindow = False
+        needInit = True
