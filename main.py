9637635b9d31ceaba4d2528ba39e6c818b1d9923
Jwischer <jakewischer@gmail.com>
2024-02-10 17:53:09 -0600

    Pushing verified backend scripts

--- main.py
+++ main.py
@@ -1,31 +1,35 @@
-import os
-from time import sleep #used for delays for mouse inputs
-from pyautogui import size as screenSize #used to get monitor resolution
-from pynput.mouse import Controller, Button #used to control mouse input
-from pynput.keyboard import Listener, Key #used to control keyboard
+import time
+from BackendPackage import GmailController, DataRetriever
 
-#Hotkey function
-def on_press(key):
-    try: #standard alphanumeric keys go here
-        if(key.char == 'a'):
-            mouse.position = (screenWidth/2,screenHeight/2)
-            sleep(0.2)
-            mouse.click(Button.left)
-            sleep(0.2)
-            mouse.scroll(0, 5)
-        if(key.char == 'r'):
-            os.startfile(os.path.join(mainFolder,"README.md")) #open readme
-    except: #special keys go here
-        if(key == Key.esc):
-            print("esc")
-            os._exit(0)
+# If modifying these scopes, delete the file token.json.
+# mail.google.com is used to access gmail services
+# googleapis.com/auth/drive is used for forms services, unneeded in this file but used elsewhere
+SCOPES = ["https://mail.google.com/","https://www.googleapis.com/auth/drive"]
+#Title of email
+EMAIL_TITLE = 'Photobooth Pictures'
+#Body of email
+EMAIL_BODY = 'This is an email from the photobooth.\nYour pictures are attached below!'
+#Url to google sheet linked with the form
+SHEET_URL = "https://docs.google.com/spreadsheets/d/1ZvP0DCTNS9V6_j5W0KnYFsm9eFivFmjWdlUvMd-8pME/edit?resourcekey#gid=2108124015"
 
-#Setup
-mainFolder = os.path.dirname(__file__)
-screenWidth, screenHeight = screenSize() #get monitor resolution
-mouse = Controller()
-keyboard = Listener(on_press=on_press)
-keyboard.start() #begin listening for keystrokes, threaded
+#NOTE: This is just for testing, remove later
+#Test receiver email
+TEST_EMAIL = "photoboothreceiver@gmail.com"
 
-while True: #Infinite loop so script wont close, hotkeys handled in thread
-    pass
+#Define a GmailController
+mailController = GmailController(SCOPES)
+
+#Define a DataReceiver
+dataController = DataRetriever(SHEET_URL)
+
+while True:
+    time.sleep(5)
+    result = dataController.checkChanges()
+    if(result):
+        email, folderId = dataController.getRecentData(1)
+        print("Sending pictures associated with key " + folderId[0] + " to email " + email[0])
+        result = mailController.sendMessage(EMAIL_TITLE, EMAIL_BODY, email[0], folderId[0])
+        if(result):
+            print("Pictures associated with key " + folderId[0] + " sent successfully")
+        else:
+            print("Pictures associated with key " + folderId[0] + " failed to send")
